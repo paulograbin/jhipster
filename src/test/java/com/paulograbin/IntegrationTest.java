20656c7a04f56b59bf27f8f622981d1144fecd94
Paulo Grolli Grabin <paulograbin@gmail.com>
2022-12-22 14:18:06 -0300

    Initial version of confirmation generated by generator-jhipster@7.9.3

---
+++ src/test/java/com/paulograbin/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.paulograbin;
+
+import com.paulograbin.ConfirmationApp;
+import com.paulograbin.config.AsyncSyncConfiguration;
+import com.paulograbin.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { ConfirmationApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
